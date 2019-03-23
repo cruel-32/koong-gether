5aa138a9d90b85f3a925e0d364d15fa70f136cdc
cruel32 <cshee8508@gmail.com>
2019-03-23 22:35:27 +0900

    쿵실이 작업물 임시저장

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const { src, dest, series, parallel, watch, lastRun } = require('gulp');
+const 
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    csscomb = require('gulp-csscomb'),
+    cssmin = require('gulp-cssmin'),
+    browsersync = require("browser-sync").create(),
+    origin = "scss",
+    project = "./";
+
+const css = () => src([`${origin}/**/*.{scss,sass}`,`!${origin}/import/**/*.{scss,sass}`], {since: lastRun(css)})
+    .pipe(sass.sync().on('error', sass.logError))
+    // .pipe(sass().on('error', sass.logError))
+    .pipe(src([`${origin}/**/*.css`,`!${origin}/_icons.css`]), {passthrough: true})
+    .pipe(autoprefixer())
+    .pipe(csscomb({
+        configPath: 'hint/.csscomb.json'
+    }))
+    .pipe(cssmin())
+    .pipe(dest(`${project}`))
+    .pipe(browsersync.stream());;
+
+
+const browserSyncInit = (done)=>{
+    browsersync.init({
+        index:'쿵실이 - 트게더.html',
+        server: {
+            baseDir: `./`,
+        },
+        port: 7777
+    },(err,bs)=>{
+        // console.log('err : ', err);
+        // console.log('server : ', bs.options.get('server'));
+        // console.log('urls : ', bs.options.get('urls'));
+    });
+    done();
+}
+
+const watcher = () => {
+    watch([`${origin}/**/*.{scss,sass.css}`], css).on('change', browsersync.reload);
+}
+
+exports.default = series(css, parallel(browserSyncInit, watcher) );
